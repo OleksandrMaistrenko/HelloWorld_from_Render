dc044ee36995fa7499bbde53791c4bca9eaa1046
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-03-13 11:36:28 +0100

    first commit

---
+++ client/src/container/Container.js
@@ -0,0 +1,12 @@
+import "./Container.css";
+import React, { useState } from "react";
+
+function Container() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Container;
